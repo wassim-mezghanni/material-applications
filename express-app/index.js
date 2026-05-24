a1a65e4aacbac485b1dd2cfd22d5ab1fda8b7e2d
Wassim Mezghanni <wassim.mezghanni@quatelio.com>
2026-05-24 16:16:11 +0200

    Use dynamic port environment variable for compatibility with cloud deployment

--- express-app/index.js
+++ express-app/index.js
@@ -5,7 +5,7 @@ app.get('/', (req, res) => {
   res.send('<h1>Hello World!</h1>')
 })
 
-const PORT = 8080
+const PORT = process.env.PORT || 8080
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
